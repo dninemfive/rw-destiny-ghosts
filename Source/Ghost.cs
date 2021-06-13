73e16e38936b0bc7177100f0dcf2725fd68f7f2f
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-06-13 13:36:33 -0700

    start sketching, but got overwhelmed

---
+++ Source/Ghost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace RWGhosts
+{
+    public class Ghost : Apparel 
+    {
+        /// <summary>
+        /// Holds a Pawn instance for use in creating <c>DirectPawnRelations</c>. Only used for its <c>Pawn_RelationsTracker</c>, otherwise ignored.
+        /// Try to make most of these fields static?
+        /// </summary>
+        public Pawn DummyPawn;
+    }
+}
